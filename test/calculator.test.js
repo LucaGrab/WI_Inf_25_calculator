96b6adfe6d794a81f3abc89cd50c55794d8d0b7d
Kristina Popp <kristina2.popp@gmail.com>
2025-10-24 15:26:17 +0200

    Absichtlich fehlschlagender Test für CI

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,5 @@
 const calc = require('../src/calculator');
-test('add', () => expect(calc.add(1,2)).toBe(3));
+test('add', () => expect(calc.add(1,2)).toBe(5));
 test('sub', () => expect(calc.sub(6,4)).toBe(2));
 test('mul', () => expect(calc.mul(2,3)).toBe(6));
 test('div', () => expect(calc.div(6,3)).toBe(2));
