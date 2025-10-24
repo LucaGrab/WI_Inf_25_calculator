cba377449e451ee1d43405ebcd43a0074bb7a5a3
Kristina Popp <kristina2.popp@gmail.com>
2025-10-24 15:04:57 +0200

    Nachtrag Test

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -6,3 +6,4 @@ test('div', () => expect(calc.div(6,3)).toBe(2));
 test('addThenMulThenDivBy3', () => expect(calc.addThenMulThenDivBy3(1,2)).toBe(3));
 test('div by zero throws', () => expect(() => calc.div(1,0)).toThrow());
 test('sqrt', () => expect(calc.sqrt(4)).toBe(2));
+test('fac', () => expect(calc.fac(5)).toBe(120));
